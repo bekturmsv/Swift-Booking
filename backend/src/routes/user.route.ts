a20701474f6becc9f434bda8f1c8e0ff9e580660
Bektur <itsbektur@gmail.com>
2024-02-17 10:43:03 +0600

    add register validation

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,39 +1,21 @@
-import express, { Request, Response } from "express";
-import User from "../models/user.model";
-import jwt from "jsonwebtoken";
+import express from "express";
+import { registerUser } from "../controllers/auth.controller";
+import { check } from "express-validator";
 
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  try {
-    let user = await User.findOne({ email: req.body.email });
-
-    if (user) {
-      res.status(400).json({ message: "User already exists!" });
-    }
-
-    user = new User(req.body);
-    await user.save();
-
-    const token = jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET_KEY as string,
-      {
-        expiresIn: "1d",
-      }
-    );
-
-    res.cookie("auth_token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 86400000,
-    });
-
-    return res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Something went wrong" });
-  }
-});
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is requried").isEmail(),
+    check(
+      "password",
+      "Password with 6 hours or more characters required"
+    ).isLength({ min: 6 }),
+  ],
+  registerUser
+);
 
 export default router;
