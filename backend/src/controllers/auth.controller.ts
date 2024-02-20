2461c37dbfdce9ff021fb7ba3056bf68834462d1
Bektur <itsbektur@gmail.com>
2024-02-20 19:34:51 +0600

    add login function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import User from "../models/user.model";
 import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
 
 export const registerUser = async (req: Request, res: Response) => {
   const errors = validationResult(req);
@@ -32,9 +33,46 @@ export const registerUser = async (req: Request, res: Response) => {
       maxAge: 86400000,
     });
 
-    return res.sendStatus(200);
+    return res.status(200).json({ userId: user.id });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ message: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+
+    res.status(200).json({ userId: user._id });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
